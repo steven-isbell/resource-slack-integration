50449b81a74be49eaa38378bfe96ffb5a52de85a
Steven Isbell <steveni_92@msn.com>
2018-09-23 22:40:20 -0500

    added serverless

--- index.ts
+++ index.ts
@@ -9,9 +9,12 @@ const web = new WebClient(token);
 
 const conversationId = process.env.CHANNEL_ID || '';
 
-web.chat
-  .postMessage({ channel: conversationId, text: 'Hello there' })
-  .then((res: Object) => {
-    console.log('Message sent: ', res);
-  })
-  .catch(console.error);
+const postResource = () =>
+  web.chat
+    .postMessage({ channel: conversationId, text: 'Hello there' })
+    .then((res: Object) => {
+      console.log('Message sent: ', res);
+    })
+    .catch(console.error);
+
+export default postResource;
