9b349ac91adec27085d93ecd5f9d01e081a5ef8a
Steven Isbell <steveni_92@msn.com>
2018-09-25 00:02:00 -0500

    added fetch

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { WebClient } from '@slack/client';
 import { config } from 'dotenv';
+import axios from 'axios';
 
 config();
 
@@ -9,11 +10,20 @@ const web = new WebClient(token);
 
 const conversationId = process.env.CHANNEL_ID || '';
 
+const fetchResource = async () => {
+  try {
+    const res = await axios.get('/api/path/to/resource');
+    return res.data;
+  } catch (e) {
+    throw new Error('Failed to Fetch Resource');
+  }
+};
+
 const postResource = async () => {
   try {
     const res = await web.chat.postMessage({
       channel: conversationId,
-      text: 'Hello there'
+      text: 'Hello World'
     });
     console.log('Message sent: ', res);
   } catch (e) {
