b20e428349934259dfc22793b8dc4836836dc594
Steven Isbell <steveni_92@msn.com>
2018-12-13 15:23:31 -0700

    fix cron

--- index.ts
+++ index.ts
@@ -3,8 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import cron from './utils/cron';
-import fetchCohorts from './utils/fetchCohorts';
 
-// cron.start()
-
-fetchCohorts();
+cron.start();
