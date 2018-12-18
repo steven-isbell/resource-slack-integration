0bb9eda7d7766f55a0d568ecac7d310887a962b7
Steven Isbell <steveni_92@msn.com>
2018-12-17 21:22:48 -0700

    updating lambda

--- index.ts
+++ index.ts
@@ -2,7 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import cron from './utils/cron';
+import fetchCohorts from './utils/fetchCohorts';
 
-// Run the process
-cron.start();
+// Export the function for Lambda
+module.exports.handleResource = (_: any, __: any, callback: Function) => {
+  try {
+    fetchCohorts();
+  } catch (e) {
+    console.error(`Failed to post resource: ${e}`);
+    return callback(e);
+  }
+};
