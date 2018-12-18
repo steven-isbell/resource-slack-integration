af7d1979a5460a343e748efd30f1d72d25118b2a
Steven Isbell <steveni_92@msn.com>
2018-12-17 21:11:50 -0700

    update cron time

--- utils/cron.ts
+++ utils/cron.ts
@@ -4,7 +4,7 @@ import fetchCohorts from './fetchCohorts';
 const { CronJob } = cron;
 
 // Run the process Monday - Friday at 1:30 PM
-const job = new CronJob('00 30 14 * * 1-5', () => {
+const job = new CronJob('00 30 13 * * 1-5', () => {
   fetchCohorts();
 });
 
