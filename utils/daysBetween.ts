f0b4fe0029121e461d1f62b2e5bf68f4b74059c3
Steven Isbell <steveni_92@msn.com>
2018-12-18 11:03:51 -0700

    Time issue

--- utils/daysBetween.ts
+++ utils/daysBetween.ts
@@ -1,16 +1,18 @@
 const daysBetween = (date1: Date, date2: Date): number => {
   // The number of milliseconds in one day
-  const ONE_DAY = 1000 * 60 * 60 * 24;
+  const ONE_DAY: number = 1000 * 60 * 60 * 24;
 
   // Convert both dates to milliseconds
-  const date1_ms = date1.getTime();
-  const date2_ms = date2.getTime();
+  const date1_ms: number = date1.getTime();
+  const date2_ms: number = date2.getTime();
 
   // Calculate the difference in milliseconds
-  const difference_ms = Math.abs(date1_ms - date2_ms);
+  const difference_ms: number = Math.abs(date1_ms - date2_ms);
 
   // Convert back to days and return
-  return Math.round(difference_ms / ONE_DAY);
+  const days: number = Math.round(difference_ms / ONE_DAY);
+
+  return days;
 };
 
 export default daysBetween;
--- utils/formatSchedule.ts
+++ utils/formatSchedule.ts
@@ -5,8 +5,8 @@ import fetchResource from './fetchResource';
 const formatSchedule = (cohorts: Cohort[]): void => {
   cohorts.forEach((cohort: Cohort) => {
     const numberOfDays: number = daysBetween(cohort.date_start, new Date());
-    const scheduleWeek: number = Math.floor(numberOfDays / 7) + 1;
-    const scheduleDay: number = Math.floor(((numberOfDays / 7) % 1) / 0.14);
+    const scheduleWeek: number = Math.ceil(numberOfDays / 7) + 1;
+    const scheduleDay: number = Math.ceil(((numberOfDays / 7) % 1) / 0.14);
     return fetchResource(cohort.slack_name, scheduleWeek, scheduleDay);
   });
 };
