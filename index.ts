22237b94e65e81f908345dbf97876200ebf1f707
Steven Isbell <steveni_92@msn.com>
2018-12-18 06:56:49 -0700

    formatting

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import fetchCohorts from './utils/fetchCohorts';
 // Export the function for Lambda
 module.exports.handleResource = (_: any, __: any, callback: Function) => {
   try {
-    fetchCohorts();
+    return fetchCohorts();
   } catch (e) {
     console.error(`Failed to post resource: ${e}`);
     return callback(e);
