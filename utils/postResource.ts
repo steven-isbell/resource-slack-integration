b902a5f4896f3415853666ffcec0491f42367502
Steven Isbell <steveni_92@msn.com>
2018-12-21 16:01:14 -0700

    added retry on fail

--- utils/postResource.ts
+++ utils/postResource.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import retry from 'async-retry';
 
 import Resource from '../types/Resource';
 
@@ -12,26 +13,34 @@ const postResource = async (
   if (!token || !hook) {
     throw new Error('.env not configured. Please add a .env file.');
   }
-  try {
-    const res = await axios.post(
-      hook,
-      {
-        // Send all the fetchedResources for the day
-        attachments: [...resources],
-        channel,
-        text:
-          resources.length > 1
-            ? 'Check out todays resources!'
-            : 'Check out todays resource!'
-      },
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
-    console.log('Message sent: ', res.data);
-    return res.data;
-  } catch (e) {
-    console.error(e);
-    return e;
-  }
+  await retry(
+    async bail => {
+      try {
+        const res = await axios.post(
+          hook,
+          {
+            // Send all the fetchedResources for the day
+            attachments: [...resources],
+            channel,
+            text:
+              resources.length > 1
+                ? 'Check out todays resources!'
+                : 'Check out todays resource!'
+          },
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        console.log('Message sent: ', res.data);
+        return res.data;
+      } catch (e) {
+        console.error(e);
+        if (e.status === 403 || e.status === '401') {
+          bail(new Error('Bad Token'));
+        }
+        return e;
+      }
+    },
+    { retries: 5 }
+  );
 };
 
 export default postResource;
