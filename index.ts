5587f0c01a5e2abc12a39fe919b0ed76ba95657f
Steven Isbell <steveni_92@msn.com>
2018-12-21 16:04:25 -0700

    add todo

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ config();
 import job from './utils/cron';
 import fetchCohorts from './utils/fetchCohorts';
 
+// TODO: Send setup items for the next day along with resources (e.g. Setup heroku account)?
 fetchCohorts();
 
 // job.start();
