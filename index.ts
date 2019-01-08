4dc9b9bca5b9f4e06109ec77b8f61a183fc00ac1
Steven Isbell <steveni_92@msn.com>
2019-01-08 15:20:13 -0700

    added winston

--- index.ts
+++ index.ts
@@ -1,9 +1,31 @@
 import { config } from 'dotenv';
+import winston from 'winston';
 
 config();
 
 import job from './utils/cron';
 import fetchCohorts from './utils/fetchCohorts';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log`
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  );
+}
 
 // TODO: Add logging
 
