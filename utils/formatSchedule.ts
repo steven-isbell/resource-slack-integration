d5de3f0d8c73308850abfa9d2b5ef8a223723f71
Steven Isbell <steveni_92@msn.com>
2019-01-08 16:15:16 -0700

    remove logs

--- utils/formatSchedule.ts
+++ utils/formatSchedule.ts
@@ -8,8 +8,6 @@ const formatSchedule = (cohorts: Cohort[]): void => {
     const numberOfDays: number = daysBetween(cohort.date_start, new Date());
     const scheduleWeek: number = Math.ceil(numberOfDays / 7);
     const scheduleDay: number = Math.round(((numberOfDays / 7) % 1) / 0.14);
-    console.log('scheduleDay: ', ((numberOfDays / 7) % 1) / 0.14);
-    console.log(scheduleDay);
     console.log('Fetching Resources');
     return fetchResource(cohort.slack_name, scheduleWeek, scheduleDay);
   });
